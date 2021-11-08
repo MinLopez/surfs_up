3ba6fbd7c9731adc3911343ab97fc7c1b8741bb0
MinLopez <mmaga006@gmail.com>
2021-11-08 14:41:23 -0800

    adding all module files

---
+++ app.py
@@ -0,0 +1,98 @@
+import datetime as dt
+import numpy as np
+import pandas as pd
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+
+from flask import Flask, jsonify
+
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+session = Session(engine)
+
+
+
+app = Flask(__name__)
+
+@app.route("/")
+
+def welcome():
+    return(
+    '''
+    Welcome to the Climate Analysis API!
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
+    ''')
+
+#Create new route 
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+   prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+   precipitation = session.query(Measurement.date, Measurement.prcp).\
+    filter(Measurement.date >= prev_year).all()
+   precip = {date: prcp for date, prcp in precipitation}
+   return jsonify(precip)
+
+   #Notice the .\ in the first line of our query? This is used to signify that we want our query to continue on the next line. 
+   #You can use the combination of .\ to shorten the length of your query line so that it extends to the next line.
+
+
+@app.route("/api/v1.0/stations")
+
+def stations():
+    results = session.query(Station.station).all()
+    stations = list(np.ravel(results))
+    return jsonify(stations=stations)
+
+
+
+@app.route("/api/v1.0/tobs")
+def temp_monthly():
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    results = session.query(Measurement.tobs).\
+      filter(Measurement.station == 'USC00519281').\
+      filter(Measurement.date >= prev_year).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps=temps)
+
+
+#because this has both start and end dates
+@app.route("/api/v1.0/temp/<start>")
+@app.route("/api/v1.0/temp/<start>/<end>")
+
+def stats(start=None, end=None):
+    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+
+    if not end:
+        results = session.query(*sel).\
+            filter(Measurement.date >= start).all()
+        temps = list(np.ravel(results))
+        return jsonify(temps)
+
+    results = session.query(*sel).\
+        filter(Measurement.date >= start).\
+        filter(Measurement.date <= end).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps)
+
+# cd "C:\Workspace\03 SQL\surfs_up"
+# set FLASK_APP=app.py
+# flask run
+# http://127.0.0.1:5000/api/v1.0/precipitation
+
+# http://localhost:5000/api/v1.0/stations
+
+# http://localhost:5000/api/v1.0/tobs
+
+# http://localhost:5000/api/v1.0/temp/2017-06-01/2017-06-30
